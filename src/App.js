2489eb7af8fd2b5d5f1d6c85a4c3fc5f94988191
Débora Teodoríco <debora.steodorico@gmail.com>
2021-09-21 18:43:24 -0300

    Tenta corrigir o problema da pasta /pages modificando o nome por completo

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import './App.css';
 import { Route, Switch } from 'react-router-dom';
-import Login from './Pages/Login';
-import Comidas from './Pages/Comidas';
+import Login from './MyPages/Login';
+import Comidas from './MyPages/Comidas';
 
 function App() {
   return (
