a747e0de331074e3de182dcd4c63b98b2e98c9a4
Débora Teodoríco <debora.steodorico@gmail.com>
2021-09-21 17:58:10 -0300

    Adicionado o requisito 9

--- src/App.js
+++ src/App.js
@@ -3,13 +3,15 @@ import './App.css';
 // import rockGlass from './images/rockGlass.svg';
 // import 'bootstrap/dist/css/bootstrap.min.css';
 import { Route, Switch } from 'react-router-dom';
-import Login from './pages/Login';
+import Login from './Pages/Login';
+import Comidas from './Pages/Comidas';
 
 function App() {
   return (
     <div>
       <Switch>
         <Route exact path="/" component={ Login } />
+        <Route exact path="/comidas" component={ Comidas } />
       </Switch>
     </div>
   );
---
+++ src/Pages/Comidas.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Header from '../components/Header';
+
+function Comidas() {
+  return (
+    <div>
+      <Header />
+      <h1>CLista de comidas</h1>
+    </div>
+  );
+}
+
+export default Comidas;
