8c611005ebcb253c0c1dcaee87e821651fa1c5c7
Débora Teodoríco <debora.steodorico@gmail.com>
2021-09-21 18:06:09 -0300

    Adicionado o requisito 9 erro lint

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import './App.css';
-// import rockGlass from './images/rockGlass.svg';
-// import 'bootstrap/dist/css/bootstrap.min.css';
 import { Route, Switch } from 'react-router-dom';
 import Login from './Pages/Login';
 import Comidas from './Pages/Comidas';
