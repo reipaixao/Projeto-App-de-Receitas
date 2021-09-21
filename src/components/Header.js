b6cf0d40c088dc5a9971baa4b46da6301cc14f78
Débora Teodoríco <debora.steodorico@gmail.com>
2021-09-21 20:24:19 -0300

    Adicionado a questao 11

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,21 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import profileIconSrc from '../images/profileIcon.svg';
 import searchIconSrc from '../images/searchIcon.svg';
 
 function Header() {
   return (
+
     <div>
-      <img src={ profileIconSrc } alt="Profile Icon" data-testid="profile-top-btn" />
+      <Link
+        to="/perfil"
+      >
+        <img src={ profileIconSrc } alt="Profile Icon" data-testid="profile-top-btn" />
+      </Link>
       <h1 data-testid="page-title">Comidas</h1>
       <img src={ searchIconSrc } alt="Search Icon" data-testid="search-top-btn" />
     </div>
+
   );
 }
 
