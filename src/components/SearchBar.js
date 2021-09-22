ea931f9b8ba9cfc729058fd934318c74f85c97f9
Débora Teodoríco <debora.steodorico@gmail.com>
2021-09-22 17:30:40 -0300

    Adicionando o requisito 13

---
+++ src/components/SearchBar.js
@@ -0,0 +1,39 @@
+import React from 'react';
+
+function SearchBar() {
+  return (
+    <div>
+      <input
+        type="radio"
+        name="search"
+        value="ingrediente"
+        data-testid="ingredient-search-radio"
+      />
+      Ingrediente
+      <br />
+      <input
+        type="radio"
+        name="search"
+        value="nome"
+        data-testid="name-search-radio"
+      />
+      Nome
+      <br />
+      <input
+        type="radio"
+        name="search"
+        value="primeira-letra"
+        data-testid="first-letter-search-radio"
+      />
+      Primeira letra
+      <br />
+      <button
+        type="button"
+        data-testid="exec-search-btn"
+      >
+        Buscar
+      </button>
+    </div>
+  );
+}
+export default SearchBar;
