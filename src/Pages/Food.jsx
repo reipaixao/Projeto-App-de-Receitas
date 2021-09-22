52d9e0bc41da01a06525c8d7d679255e4bf16de6
Gean634N <gean.a.l.c@gmail.com>
2021-09-22 14:46:49 -0400

    :package: requisito 19

--- src/Pages/Food.jsx
+++ src/Pages/Food.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Footer from '../components/Footer';
 import Header from '../components/Header';
 
 function Food() {
@@ -6,6 +7,7 @@ function Food() {
     <div>
       <Header title="Comidas" withSearchButton />
       <h1>Lista de comidas</h1>
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import drinkIcon from '../images/drinkIcon.svg';
+import mealIcon from '../images/mealIcon.svg'
+import exploreIcon from '../images/exploreIcon.svg';
+import './footer.css';
+
+function Footer({ title, withSearchButton }) {
+  return (
+    <div className="footer__container" data-testid="footer">
+      <Link
+        className="footer__link drinks"
+        to="../Pages/Drinks"
+      >
+        <img src={ drinkIcon } alt="link para a pagina de Drinks" data-testid="drinks-bottom-btn" />
+      </Link>
+      <Link
+        className="footer__link explorer"
+        to="../Pages/ExploreScreen"
+      >
+        <img src={ exploreIcon } alt="link para a página de explorar" data-testid="explore-bottom-btn" />
+      </Link>
+      <Link
+        className="footer__link food"
+        to="../Pages/Food"
+      >
+        <img src={ mealIcon } alt="link para a página de comidas " data-testid="food-bottom-btn" />
+      </Link>
+      
+    </div>
+  );
+}
+
+export default Footer;
