7a3146aac445040007829b1426e8cd318e515d92
Gean634N <gean.a.l.c@gmail.com>
2021-09-22 17:25:41 -0400

    :package: requisito 21

--- src/Pages/Drinks.jsx
+++ src/Pages/Drinks.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Header from '../components/Header';
+import Footer from '../components/Footer';
 
 function Drinks() {
   return (
     <div>
       <Header title="Bebidas" withSearchButton />
       <h1>Lista de bebidas</h1>
+      <Footer />
     </div>
   );
 }
