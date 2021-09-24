aa0f95da2b5092993933ca04f482505510684dfa
Reinaldo Paixao <reipaixao.b@gmail.com>
2021-09-24 07:29:51 -0300

    Adiciona requisitos 67, 68, 69

--- src/components/RecipeDetailCard.jsx
+++ src/components/RecipeDetailCard.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+// https://reactrouter.com/web/api/Hooks
 import { useLocation, useParams } from 'react-router-dom';
 import { getMealById, getDrinkById } from '../services/Api';
 
@@ -14,15 +15,18 @@ function RecipeDetail() {
     const getRecipe = () => {
       if (pathname.includes('comida')) {
         setRecipe(getMealById(IdMeal));
+        // console.log(recipe);
       } else setRecipe(getDrinkById(IdDrink));
     };
     getRecipe();
-  }, [recipe, pathname, id]);
+  }, [pathname, id]);
+
+  console.log(recipe);
 
   return (
     <section>
-      <img data-testid="recipe-photo" alt="recipe" />
-      <h2 data-testid="recipe-title"> </h2>
+      {/* <img src={recipe.strDrinkThumb} data-testid="recipe-photo" alt="recipe" />
+      <h2 data-testid="recipe-title">{recipe.strMeal || recipe.strDrink}</h2> */}
       <button data-testid="share-btn" type="button">Share</button>
       <button data-testid="favorite-btn" type="button">Favorite</button>
       <p
