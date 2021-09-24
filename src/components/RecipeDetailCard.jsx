7e72e8f1723da3348d2d196fd683cf18d0c81a41
Reinaldo Paixao <reipaixao.b@gmail.com>
2021-09-24 18:09:23 -0300

    Requisito 35, com id fixo

--- src/components/RecipeDetailCard.jsx
+++ src/components/RecipeDetailCard.jsx
@@ -1,55 +1,101 @@
 import React, { useEffect, useState } from 'react';
 import { useLocation, useParams } from 'react-router-dom';
 import { getMealById, getDrinkById } from '../services/Api';
+import shareIcon from '../images/shareIcon.svg';
 
 function RecipeDetail() {
-  const index = 0;
-  const IdMeal = 52771;
-  const IdDrink = 178319;
+  const idMeal = 52771;
+  const idDrink = 178319;
   const { pathname } = useLocation();
   const { id } = useParams();
-  const [recipe, setRecipe] = useState();
+  const [recipe, setRecipe] = useState([]);
+  const [ingredients, setIngredients] = useState([]);
+  // Feito conjuntamento no plantão com o André
+  const fillIngredients = (meal) => {
+    const strMeal = Object.entries(meal[0]);
+    const strIngredient = strMeal.filter(([chave, valor]) => chave
+      .includes('strIngredient') && valor);
+    const strMeasure = strMeal.filter(([chave, valor]) => chave
+      .includes('strMeasure') && valor);
+    return strIngredient.map((item, index) => `${item[1]} - ${strMeasure[index][1]}`);
+  };
 
   useEffect(() => {
-    const getRecipe = () => {
-      if (pathname.includes('comida')) {
-        setRecipe(getMealById(IdMeal));
-      } else setRecipe(getDrinkById(IdDrink));
+    const getRecipe = async () => {
+      if (pathname.includes('comidas')) {
+        setRecipe(await getMealById(idMeal));
+        const meal = await getMealById(idMeal);
+        setIngredients(fillIngredients(meal));
+      } else {
+        setRecipe(await getDrinkById(idDrink));
+        const drink = await getDrinkById(idDrink);
+        setIngredients(fillIngredients(drink));
+      }
     };
     getRecipe();
-  }, [recipe, pathname, id]);
+  }, [id, pathname]);
+
+  const index = 0;
 
   return (
-    <section>
-      <img data-testid="recipe-photo" alt="recipe" />
-      <h2 data-testid="recipe-title"> </h2>
-      <button data-testid="share-btn" type="button">Share</button>
-      <button data-testid="favorite-btn" type="button">Favorite</button>
-      <p
-        data-testid="recipe-category"
-      >
-        Categoria
-      </p>
-      <p
-        data-testid="instructions"
-      >
-        Instruções
-      </p>
-      <p
-        data-testid={ `${index}-ingredient-name-and-measure` }
-      >
-        Ingredientes
-      </p>
-      <span
-        data-testid="video"
-      />
-      <p
-        data-testid={ `${index}-recomendation-card` }
-      >
-        RecomendationCard
-      </p>
-      <button type="button" data-testid="start-recipe-btn">Iniciar Receita</button>
-    </section>
+    <div>
+      {/* Com auxilio do Murilo Rainho */}
+      {(recipe.length === 1) && (
+        <section>
+          <img
+            style={ { width: '200px' } }
+            src={ recipe[0].strMealThumb || recipe[0].strDrinkThumb }
+            data-testid="recipe-photo"
+            alt="cocktail"
+          />
+          <h2 data-testid="recipe-title">{recipe[0].strMeal || recipe[0].strDrink}</h2>
+          <button
+            type="button"
+            data-testid="share-btn"
+          >
+            <img src={ shareIcon } alt="compartilhar" />
+          </button>
+          <button data-testid="favorite-btn" type="button">Favorite</button>
+          <p
+            data-testid="recipe-category"
+          >
+            {recipe[0].strCategory === 'Cocktail'
+              ? recipe[0].strAlcoholic : recipe[0].strCategory}
+          </p>
+          <ul>
+            { ingredients.map((ingredient, idx) => (
+              <li
+                data-testid={ `${idx}-ingredient-name-and-measure` }
+                key={ `${idx}` }
+              >
+                { ingredient }
+              </li>
+            ))}
+          </ul>
+          <p
+            data-testid="instructions"
+          >
+            Instruções
+            { recipe[0].strInstructions }
+          </p>
+          {/* https://stackoverflow.com/questions/25661182/embed-youtube-video-refused-to-display-in-a-frame-because-it-set-x-frame-opti */}
+          {pathname.includes('comidas') ? (
+            <iframe
+              width="200"
+              height="120"
+              src={ `${(recipe[0].strYoutube).replace('watch?v=', 'embed/')}` }
+              title="video"
+              data-testid="video"
+            />) : null}
+          <p
+            data-testid={ `${index}-recomendation-card` }
+          >
+            RecomendationCard
+          </p>
+          <button type="button" data-testid="start-recipe-btn">Iniciar Receita</button>
+        </section>
+      )}
+    </div>
   );
 }
 
