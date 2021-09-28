1b0eacad27e28b2001427ccf6f68099e95157676
Elaine Moreira <elainefelipemoreira@gmail.com>
2021-09-28 09:30:28 -0300

    Adiciona requisitos do 70 ao 74 modificados

--- src/Pages/ExploreFood.jsx
+++ src/Pages/ExploreFood.jsx
@@ -1,14 +1,51 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useHistory, Link } from 'react-router-dom';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
-import ButtonsExplorer from '../components/ButtonsExplorer';
+import { getRandomMeal } from '../services/Api';
 
 function ExploreFood() {
+  const [randomMeal, setRandomMeal] = useState();
+  const history = useHistory();
+
+  const handleClick = async () => {
+    const result = await getRandomMeal();
+    setRandomMeal(result.meals[0].idMeal);
+  };
+
+  useEffect(() => {
+    if (randomMeal) history.push(`/comidas/${randomMeal}`);
+  }, [randomMeal]);
+
   return (
     <div>
-      <Header title="Explorar Comidas" withSearchButton={ false } />
-      <h1>Explorar Comidas</h1>
-      <ButtonsExplorer page="food" />
+      Pagina SearchMeals
+      <Header title="Explorar Comidas" />
+      <div>
+        <Link to="/explorar/comidas/ingredientes">
+          <button
+            type="button"
+            data-testid="explore-by-ingredient"
+          >
+            Por Ingredientes
+          </button>
+        </Link>
+        <Link to="/explorar/comidas/area">
+          <button
+            type="button"
+            data-testid="explore-by-area"
+          >
+            Por Local de Origem
+          </button>
+        </Link>
+        <button
+          type="button"
+          data-testid="explore-surprise"
+          onClick={ handleClick }
+        >
+          Me Surpreenda!
+        </button>
+      </div>
       <Footer />
     </div>
   );
