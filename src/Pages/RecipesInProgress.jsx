ac4e4e19ccfbb59a2ebc95bf4197ee799586a1a1
Débora Teodoríco <debora.steodorico@gmail.com>
2021-09-30 16:24:32 -0300

    adiciona requisitos 48 e 49

--- src/Pages/RecipesInProgress.jsx
+++ src/Pages/RecipesInProgress.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import RecipeDetail from '../components/RecipeDetailCard';
 import DetailsButtons from '../components/DetailsButtons';
 
@@ -7,7 +8,9 @@ function RecipesInProgress() {
     <div>
       <RecipeDetail withStartRecipesButton={ false } />
       <DetailsButtons />
-      <button type="button" data-testid="finish-recipe-btn">Finalizar Receita</button>
+      <Link to="/receitas-feitas">
+        <button type="button" data-testid="finish-recipe-btn">Finalizar Receita</button>
+      </Link>
     </div>
   );
 }
